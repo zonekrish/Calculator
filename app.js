561bb36482d360c371eea4364b4122a4f938392e
zonekrish <krishna.rao0626@gmail.com>
2024-06-26 08:09:20 -0500

    Work on add function

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+var oper = "";
+var final = 0;
+
 function displayItem(value) {
     document.getElementById("disp").textContent+=value;
 
 }
 
+function add() {
+    let val = parseInt(document.getElementById("disp").textContent);
+    oper = "add";
+    final = val;
+    document.getElementById("disp").textContent = 0;
+
+}
+
 function clear() {
     document.getElementById("disp").innerHTML="";
+}
+
+function equal() {
+    let val = parseInt(document.getElementById("disp").textContent);
+    switch (oper) {
+        case "add":
+            final+=val;
+
+
+    }
+
+    document.getElementById("disp").innerHTML=""+final;
+
 }
